0bded321b399c4095a09e6e4787c22d8719fa31a
poulpy666 <lilopolo912@gmail.com>
2026-08-24 11:41:32 +0200

    bl

---
+++ init.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/bash
+
+set -eu -o pipefail
+
+if [ ! -d ".git" ]; then
+    git init
+fi
+
+{
+    echo "*.tfvars"
+    echo ".env"
+    echo ".terraform"
+    echo "*tfstate"
+    echo "*tfstate.backup"
+    echo "*.tfstate"
+    echo "tfplan"
+    echo "*tfplan"
+
+}>>.gitignore
+
+mkdir -p infra/envs/dev
+cd infra/envs/dev
+touch "main.tf" "variables.tf" "versions.tf" "outputs.tf" "providers.tf" "dev.aut.tfvars"
+cd ../..
+mkdir -p infra/modules/compute
+mkdir -p infra/modules/security_group
+
+
+for f in infra/modules/compute infra/modules/security_group; do
+    cd "$f"
+    touch "main.tf" "variables.tf" "versions.tf" "outputs.tf"
+done
