ce6f420c05c753fc30be367f5bf59357d16f4b1e
poulpy666 <lilopolo912@gmail.com>
2026-08-24 12:54:24 +0200

    F

--- make/common.mk
+++ make/common.mk
@@ -1,6 +1,12 @@
 .ONESHELL:
 SHELL := /usr/bin/bash
 .SHELLFLAGS := -euo pipefail -c
+.DEFAULT_GOAL := help
+INFO_COLOR := \033[36;1m
+ERROR_COLOR := \033[31;1m
+SUCCESS_COLOR := \033[32;1m
+WARNING_COLOR := \033[33;1m
+RESET_COLOR := \033[m
 
 help: ##shows this help
 	@grep -hE "^[a-zA-Z0-9_-]+:.*?##" $(MAKEFILE_LIST)
