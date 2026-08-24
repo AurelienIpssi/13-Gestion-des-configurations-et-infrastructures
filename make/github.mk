057a9b7c6e152c93ca68d89834bfd6519857aa46
poulpy666 <lilopolo912@gmail.com>
2026-08-24 15:19:49 +0200

    fr

---
+++ make/github.mk
@@ -0,0 +1,5 @@
+REPO_NAME :$(shell basename `git rev-parse --show-toplevel`)
+VISIBILITY ?= private
+.PHONY: gh.create
+gh.create:
+@gh repo create $(REPO_NAME) --$(VISIBILITY) --source=. --remote=origin --push
