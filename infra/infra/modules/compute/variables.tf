2f0c2e7c06e36ce690128a88a9970952c2f1b24f
poulpy666 <lilopolo912@gmail.com>
2026-08-25 14:52:32 +0200

    f

--- infra/infra/modules/compute/variables.tf
+++ infra/infra/modules/compute/variables.tf
@@ -3,15 +3,12 @@ type        = string
 default     = "AMI of EC2 Instance"
 #default     = ""
 }
-
 variable "instance_type" {
 type        = string
 }
-
 variable "subnet_id" {
 type        = string
 }
-
 variable "sg_ids" {
 type        = list(string)
 }
