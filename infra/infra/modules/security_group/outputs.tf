617fd4c70c40b607e16f3219210c8e92317298b1
AurelienG0 <aurelien.goncalves@outlook.fr>
2026-08-26 16:09:32 +0200

    Add sg_name and sg_arn outputs to security_group module

--- infra/infra/modules/security_group/outputs.tf
+++ infra/infra/modules/security_group/outputs.tf
@@ -1,3 +1,11 @@
 output "sg_id" {
   value = aws_security_group.this.id
 }
+
+output "sg_name" {
+  value = aws_security_group.this.name
+}
+
+output "sg_arn" {
+  value = aws_security_group.this.arn
+}
