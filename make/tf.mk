b20217f070d6d7b7a76092c93ce4e0217ad9b990
AurelienG0 <aurelien.goncalves@outlook.fr>
2026-08-26 15:57:37 +0200

    Quote -chdir path in tf.mk targets
    The repo lives under a path with spaces (OneDrive - NDSI/...), which
    split the unquoted \$(TF_ENV_DIR)/\$(INFRA_DIR) into multiple argv
    entries and made terraform misread part of the path as a subcommand.

--- make/tf.mk
+++ make/tf.mk
@@ -2,10 +2,10 @@ INFRA_DIR:=	infra/
 
 .PHONY:	tf.fmt.ci tf.fmt tf.init
 tf.fmt:
-	@terraform -chdir=$(INFRA_DIR) fmt -recursive -diff
+	@terraform -chdir="$(INFRA_DIR)" fmt -recursive -diff
 
 tf.fmt.ci:
-	@terraform -chdir=$(INFRA_DIR) fmt -recursive -check
+	@terraform -chdir="$(INFRA_DIR)" fmt -recursive -check
 
 tf.init: ##terraform init for the current ENV
-	@terraform -chdir=$(TF_ENV_DIR) init -lock=false -input=false -upgrade
+	@terraform -chdir="$(TF_ENV_DIR)" init -lock=false -input=false -upgrade
