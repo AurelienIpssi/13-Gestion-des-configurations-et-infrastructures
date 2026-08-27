f41e3e1adcf6e2e9862ba3bd04eb85faecd0c9b9
AurelienG0 <aurelien.goncalves@outlook.fr>
2026-08-27 09:52:31 +0200

    Add make tf.checkov (via Docker, native checkov blocked by this machine's application control policy)

--- make/tf.mk
+++ make/tf.mk
@@ -1,6 +1,6 @@
 INFRA_DIR:=	infra/
 
-.PHONY:	tf.fmt.ci tf.fmt tf.init tf.lint
+.PHONY:	tf.fmt.ci tf.fmt tf.init tf.lint tf.checkov
 tf.fmt:
 	@terraform -chdir="$(INFRA_DIR)" fmt -recursive -diff
 
@@ -10,5 +10,8 @@ tf.fmt.ci:
 tf.init: ##terraform init for the current ENV
 	@terraform -chdir="$(TF_ENV_DIR)" init -lock=false -input=false -upgrade
 
-tf.lint: ##scan the terraform code for security misconfigurations
+tf.lint: ##scan the terraform code for security misconfigurations (trivy)
 	@trivy config "$(INFRA_DIR)"
+
+tf.checkov: ##scan the terraform code for security misconfigurations (checkov, via Docker)
+	@MSYS_NO_PATHCONV=1 docker run --rm -v "$(CURDIR)/infra:/tf" bridgecrew/checkov:latest -d /tf --compact --quiet
